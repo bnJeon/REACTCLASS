2c54bf2b441e07b0eee048b7d21f8b700e6bc7a4
bnJeon <bn.jeon9210@gmail.com>
2024-04-26 17:54:34 +0900

    mysql_Server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,32 @@
 const express = require("express");
-const app = express();
+const cors    = require("cors");    // npm i cors | yarn add cors
+const mysql   = require("mysql");   // npm i mysql | yarn add mysql
+const app     = express();
+const PORT    = 3001; // 포트번호 설정
+
+// MySQL 연결
+const db = mysql.createPool({
+    host: "localhost:3306", // 호스트
+    user: "root",      // 데이터베이스 계정
+    password: "1234",      // 데이터베이스 비밀번호
+    database: "react",  // 사용할 데이터베이스
+});
+
+app.use(cors({
+    origin: "*",                // 출처 허용 옵션
+    credentials: true,          // 응답 헤더에 Access-Control-Allow-Credentials 추가
+    optionsSuccessStatus: 200,  // 응답 상태 200으로 설정
+}))
+
+app.use(cors({
+    origin: "*",                // 출처 허용 옵션
+    credentials: true,          // 응답 헤더에 Access-Control-Allow-Credentials 추가
+    optionsSuccessStatus: 200,  // 응답 상태 200으로 설정
+}))
+
+// post 요청 시 값을 객체로 바꿔줌
+app.use(express.urlencoded({ extended: true })) 
+
 const test = require("./Router/test");
 
 app.use("/api", test);
